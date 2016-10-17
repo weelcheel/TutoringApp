e8e979fd621c7af6b6077266ef0d0b2ab095d953
Will Hagler <will.hagler@gmail.com>
2016-10-17 00:26:58 -0500

    Dependencies and Testing Sign-In
    Added the necessary dependencies to the project and tested the Firebase
    authenticator with our app ID in the Main Activity.

--- app/src/main/java/com/csce/tutorapp/MainActivity.java
+++ app/src/main/java/com/csce/tutorapp/MainActivity.java
@@ -2,12 +2,36 @@ package com.csce.tutorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* authenticators that communicate with Firebase */
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize firebase authenticator
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        if (firebaseUser == null)
+        {
+            //no user signed in, so launch a sign in activity
+            Log.d("user", "no user currently signed in");
+        }
+        else
+        {
+            //there is a user signed in, so load their home page
+            Log.d("user", "user is signed in");
+        }
     }
 }
