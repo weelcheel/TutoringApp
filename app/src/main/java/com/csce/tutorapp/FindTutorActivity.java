3cbceec40d6975604350bd2f06d57861c9d1ead6
joelash101 <joelash101@icloud.com>
2016-10-23 14:56:37 -0500

    new file:   .idea/encodings.xml 	new file:   app/src/main/java/com/csce/tutorapp/FindTutorActivity.java 	new file:   app/src/main/res/layout/activity_find_tutor.xml 	new file:   app/src/main/res/values/ids.xml 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/csce/tutorapp/FindTutorActivity.java 	modified:   app/src/main/res/layout/activity_find_tutor.xml 	modified:   app/src/main/res/values/ids.xml 	modified:   app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/csce/tutorapp/FindTutorActivity.java
@@ -0,0 +1,13 @@
+package com.csce.tutorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindTutorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_tutor);
+    }
+}
