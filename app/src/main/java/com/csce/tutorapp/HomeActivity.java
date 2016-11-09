f447468f9ba7f7b325ad7a86aa6f913f0657eee6
Will Hagler <will.hagler@gmail.com>
2016-11-09 12:00:48 -0600

    User Name for Home Screen
    Added a global user account for the Home screen and successfully
    populate that user account with details downloaded from Firebase
    database.

--- app/src/main/java/com/csce/tutorapp/HomeActivity.java
+++ app/src/main/java/com/csce/tutorapp/HomeActivity.java
@@ -6,16 +6,29 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
 
     /* signout button component */
     private Button signoutButton;
+
+    /* profile name */
+    private TextView profileName;
+
+    /* user profile that is currently signed in */
+    private User signedInUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,7 @@ public class HomeActivity extends AppCompatActivity {
 
         //assign button
         signoutButton = (Button) findViewById(R.id.signoutBtn);
+        profileName = (TextView) findViewById(R.id.profileName);
 
         //set button text based on sign in status
         if (FirebaseUtility.getCurrentFirebaseUser() == null)
@@ -41,6 +55,44 @@ public class HomeActivity extends AppCompatActivity {
                 finish();
             }
         });
-    }
 
+        final DatabaseReference userProfileDb = FirebaseDatabase.getInstance().getReference("users").child(FirebaseUtility.getCurrentFirebaseUser().getUid());
+        userProfileDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                signedInUser = dataSnapshot.getValue(User.class);
+                if (signedInUser != null) {
+                    Log.d("profile", "Value is: " + signedInUser.getEmail());
+                    if (signedInUser.getIsProfileCreated()) {
+                        //populate home screen here
+                        profileName.setText(signedInUser.getFirstName() + " " + signedInUser.getLastName());
+                    } else {
+                        Intent userprofileIntent = new Intent(HomeActivity.this, UserProfileActivity.class);
+                        userprofileIntent.putExtra(FirebaseUtility.INTENT_USER_PATH, signedInUser);
+                        startActivity(userprofileIntent);
+                        finish();
+                    }
+                } else {
+                    User newUser = new User(FirebaseUtility.getCurrentFirebaseUser().getUid());
+                    //HashMap<String, User> userMap = new HashMap<>();
+                    //userMap.put(newUser.getID(), newUser);
+                    FirebaseUtility.updateUser(newUser);
+
+                    Intent userprofileIntent = new Intent(HomeActivity.this, UserProfileActivity.class);
+                    userprofileIntent.putExtra(FirebaseUtility.INTENT_USER_PATH, newUser);
+                    startActivity(userprofileIntent);
+                    finish();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("profile", "Failed to read value.", error.toException());
+                profileName.setText("ERROR! :(");
+            }
+        });
+    }
 }
